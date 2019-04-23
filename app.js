2b403557ae2208ddd50f93ead321b8b969fc291c
celeriss@gmail.com <Cel85>
2019-04-23 12:20:49 +0200

    adding custom commands

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 
 const chalk = require('chalk');
 const getNotes = require('./notes');
+const yargs = require('yargs');
 
-const command = process.argv [2];
+//customize yargs
 
-if(command === 'add') {
-    console.log('ADDING NODE');
-}else if (command === 'remove'){
-    console.log('removing node :/ss');
-}
+yargs.version('1.1.0.');
+
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'add new note',
+    handler: function(){
+        console.log('adding new note')
+    }
+});
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'removes a note',
+    handler: function(){
+        console.log('removing note...')
+    }
+});
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function(){
+        console.log("Listing notes...");
+    }
+});
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reads notes',
+    handler: function(){
+        console.log("Reading notes...");
+    }
+});
+
+console.log(yargs.argv);
