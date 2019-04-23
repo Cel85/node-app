dd84e79b951624fc42fa46b0208d4af703c2c597
celeriss@gmail.com <Cel85>
2019-04-23 11:13:37 +0200

    prva

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const chalk = require('chalk');
+const getNotes = require('./notes');
+
+const command = process.argv [2];
+
+if(command === 'add') {
+    console.log('ADDING NODE');
+}else if (command === 'remove'){
+    console.log('removing node');
+}
