1a55a90f23bf4956c71ffe395488d0a89dcffb3e
Marko Rašeta <marko.raseta@moj-posao.net>
2019-05-23 14:08:48 +0200

    git

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,11 +2,37 @@ const request = require('request')
 
 
 
-const url = 'https://api.darksky.net/forecast/084add42797cda53764666754b63824a/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/084add42797cda53764666754b63824a/45.45,15.95?units=si&lang=hr'
+const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Zagreb.json?access_token=pk.eyJ1IjoiY2VsZXJpcyIsImEiOiJjanZ6YzFobXMwbXl2NGJtcW45NjVjaWh2In0.TERSrWgQTNLkJUfqODkJQg&limit=1'
+
+
+
+// request({
+//     url: url,
+//     json: true
+// }, (error, response, body) => {
+//     if (error) {
+//         console.log("Unable to connect");
+//     } else if (body.code === 400) {
+//         console.log(body.error);
+//     } else {
+
+//         console.log(body.daily.data[0].summary + ' Trenutno je ' + body.currently.temperature + ' stupnjeva s ' + body.currently.precipProbability + '% šanse za kisu.');
+//     }
+// })
+
 
 request({
-    url: url
+
+    url: geocodeUrl,
+    json: true
 }, (error, response, body) => {
-    const data = JSON.parse(body)
-    console.log(data.hourly.data)
-})
+    const longitude = body.features[0].center[0];
+    const latitude = body.features[0].center[1];
+
+    if (error) {
+        console.log("Unable to connect");
+    }
+    console.log(longitude + ',' + latitude);
+
+});
